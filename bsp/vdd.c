16dd22559b6c17483dfc363236161248dd9785ea
Andrey Chilikin <achilikin@gmail.com>
2023-10-15 12:23:20 +0100

    Read bandgat at init time only

--- bsp/vdd.c
+++ bsp/vdd.c
@@ -3,21 +3,28 @@
 #include "adc.h"
 #include "iap.h"
 
+static uint16_t iap_bgap = 0;
+
 uint16_t adc_get_vdd(uint8_t mode)
 {
-	uint16_t bgap = iap_read_uid(IAP_UID_SIZE);
-	bgap <<= 4;
-	bgap |= iap_read_uid(IAP_UID_SIZE + 1) & 0x0F;
-	if (mode == ADC_GET_RAW_BGAP)
-		return bgap;
+	if (iap_bgap == 0) {
+		iap_bgap = iap_read_uid(IAP_UID_SIZE);
+		iap_bgap <<= 4;
+		iap_bgap |= iap_read_uid(IAP_UID_SIZE + 1) & 0x0F;
+	}
 
-	float fgap = (float)bgap * 3072.0;
+	if (mode == ADC_GET_RAW_BGAP)
+		return iap_bgap;
+
+	float fgap = (float)iap_bgap * 3072.0;
 	fgap /= 4096.0;
-	bgap = (uint16_t)fgap;
+
+	uint16_t bgap = (uint16_t)fgap;
 	if (mode == ADC_GET_BGAP)
 		return bgap;
 
 	/* mode == ADC_GET_VDD */
+	/* read ADC synchronously, takes up to 400us */
 	Enable_ADC_BandGap;
 	for (uint8_t i = 0; i < 5; i++) {
 		clr_ADCF;
